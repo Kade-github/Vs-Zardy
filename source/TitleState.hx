c899f38c553f065aa8fdf3c4f54ad0a666331421
VegethYT <84522055+VegethYT@users.noreply.github.com>
2022-09-15 21:27:36 +0200

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -291,14 +291,6 @@ class TitleState extends MusicBeatState
 
 		if (pressedEnter && !transitioning && skippedIntro)
 		{
-			#if !switch
-			NGio.unlockMedal(60960);
-
-			// If it's Friday according to da clock
-			if (Date.now().getDay() == 5)
-				NGio.unlockMedal(61034);
-			#end
-
 			if (FlxG.save.data.flashing)
 				titleText.animation.play('press');
 
